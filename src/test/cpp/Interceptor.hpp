c2a9fe4bbfd23e1fe5b4f5552422cc8d0539aa3c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-01 19:17:46 -0300

    Move methods to be inside of class template body

--- src/test/cpp/Interceptor.hpp
+++ src/test/cpp/Interceptor.hpp
@@ -11,38 +11,28 @@ private:
 
 public:
     Interceptor(O*& intercepted, R* replacement,
-            bool deleteReplacementInDestructor = true);
-    ~Interceptor();
+            bool deleteReplacementInDestructor = true)
+            : intercepted(intercepted) {
+        this->deleteReplacementInDestructor = deleteReplacementInDestructor;
+        this->replacement = replacement;
+        original = intercepted;
+        intercepted = replacement;
+    }
 
-    O* getOriginal();
-    R* getReplacement();
+    ~Interceptor() {
+        intercepted = original;
+
+        if (deleteReplacementInDestructor)
+            delete replacement;
+    }
+
+    O* getOriginal() {
+        return original;
+    }
+
+    R* getReplacement() {
+        return replacement;
+    }
 };
-
-template <class O, class R>
-Interceptor<O, R>::Interceptor(O*& intercepted, R* replacement,
-        bool deleteReplacementInDestructor) : intercepted(intercepted) {
-    this->deleteReplacementInDestructor = deleteReplacementInDestructor;
-    this->replacement = replacement;
-    original = intercepted;
-    intercepted = replacement;
-}
-
-template <class O, class R>
-Interceptor<O, R>::~Interceptor() {
-    intercepted = original;
-
-    if (deleteReplacementInDestructor)
-        delete replacement;
-}
-
-template <class O, class R>
-O* Interceptor<O, R>::getOriginal() {
-    return original;
-}
-
-template <class O, class R>
-R* Interceptor<O, R>::getReplacement() {
-    return replacement;
-}
 
 #endif
