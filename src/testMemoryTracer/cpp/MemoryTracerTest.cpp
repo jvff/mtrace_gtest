a41dbf46f0362a612340796480458023c95e7997
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-04 17:55:17 -0300

    Refactor MemoryTracerTest
    - Create test fixture class
    - Move common setup and tear down code into the new class
    - Separate the code into separate methods for easier readability

--- src/testMemoryTracer/cpp/MemoryTracerTest.cpp
+++ src/testMemoryTracer/cpp/MemoryTracerTest.cpp
@@ -1,63 +1,15 @@
-#include <errno.h>
-#include <fstream>
-#include <mcheck.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include "MemoryTracerTest.hpp"
 
-#include <gtest/gtest.h>
-
-#include "MemoryTracer.hpp"
-
-TEST(MemoryTracerTest, startTrace) {
-    const std::string envVarName = "MALLOC_TRACE";
-    const std::string mtraceFile = "/tmp/MemoryTracerTest_startTrace.mtrace";
-    const std::string constEnvironmentString = envVarName + "=" + mtraceFile;
-    char* environmentString = strdup(constEnvironmentString.c_str());
-    struct stat info;
-
-    MemoryTracer tracer;
-
-    putenv(environmentString);
-
-    tracer.start();
+TEST_F(MemoryTracerTest, startTrace) {
+    memoryTracer.start();
     muntrace();
 
-    EXPECT_EQ(0, stat(mtraceFile.c_str(), &info));
-    EXPECT_TRUE(S_ISREG(info.st_mode));
-
-    unlink(mtraceFile.c_str());
-    unsetenv(envVarName.c_str());
-    free(environmentString);
+    checkThatMtraceFileIsValid();
 }
 
-TEST(MemoryTracerTest, stopTrace) {
-    const std::string envVarName = "MALLOC_TRACE";
-    const std::string mtraceFile = "/tmp/MemoryTracerTest_stopTrace.mtrace";
-    const std::string constEnvironmentString = envVarName + "=" + mtraceFile;
-    char* environmentString = strdup(constEnvironmentString.c_str());
-    struct stat info;
-
-    MemoryTracer tracer;
-
-    putenv(environmentString);
-
+TEST_F(MemoryTracerTest, stopTrace) {
     mtrace();
-    tracer.stop();
+    memoryTracer.stop();
 
-    EXPECT_EQ(0, stat(mtraceFile.c_str(), &info));
-    EXPECT_TRUE(S_ISREG(info.st_mode));
-
-    std::ifstream traceFile(mtraceFile);
-    std::string line;
-    std::string lastLine;
-
-    while (std::getline(traceFile, line))
-        lastLine = line;
-
-    EXPECT_STREQ("= End", lastLine.c_str());
-
-    unlink(mtraceFile.c_str());
-    unsetenv(envVarName.c_str());
-    free(environmentString);
+    checkThatMtraceFileIsValid();
 }
---
+++ src/testMemoryTracer/cpp/MemoryTracerTest.hpp
@@ -0,0 +1,88 @@
+#ifndef MEMORY_TRACE_TEST_HPP
+#define MEMORY_TRACE_TEST_HPP
+
+#include <errno.h>
+#include <fstream>
+#include <mcheck.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <gtest/gtest.h>
+
+#include "MemoryTracer.hpp"
+
+class MemoryTracerTest : public testing::Test {
+private:
+    char* environmentString;
+    std::string envVarName;
+    std::string mtraceFileName;
+
+protected:
+    MemoryTracer memoryTracer;
+
+protected:
+    void SetUp() {
+        setUpMtraceFileName();
+        setUpEnvironmentVariable();
+    }
+
+    void TearDown() {
+        tearDownEnvironmentVariable();
+        deleteMtraceFile();
+    }
+
+    void checkThatMtraceFileIsValid() {
+        checkThatMtraceFileExists();
+        checkThatMtraceFileIsComplete();
+    }
+
+    void checkThatMtraceFileExists() {
+        struct stat info;
+
+        EXPECT_EQ(0, stat(mtraceFileName.c_str(), &info));
+        EXPECT_TRUE(S_ISREG(info.st_mode));
+    }
+
+    void checkThatMtraceFileIsComplete() {
+        std::ifstream mtraceFile(mtraceFileName);
+        std::string line;
+        std::string lastLine;
+
+        while (std::getline(mtraceFile, line))
+            lastLine = line;
+
+        EXPECT_STREQ("= End", lastLine.c_str());
+    }
+
+private:
+    void setUpMtraceFileName() {
+        const testing::UnitTest* unitTest = testing::UnitTest::GetInstance();
+	const testing::TestInfo* testInfo = unitTest->current_test_info();
+        const std::string testCaseName = testInfo->name();
+        const std::string testName = testInfo->name();
+        const std::string fileSuffix = ".mtrace";
+
+        mtraceFileName = testCaseName + "_" + testName + fileSuffix;
+    }
+
+    void setUpEnvironmentVariable() {
+        envVarName = "MALLOC_TRACE";
+        const std::string envStr = envVarName + "=" + mtraceFileName;
+
+        environmentString = strdup(envStr.c_str());
+
+        putenv(environmentString);
+    }
+
+    void tearDownEnvironmentVariable() {
+        unsetenv(envVarName.c_str());
+        free(environmentString);
+    }
+
+    void deleteMtraceFile() {
+        unlink(mtraceFileName.c_str());
+    }
+};
+
+#endif
