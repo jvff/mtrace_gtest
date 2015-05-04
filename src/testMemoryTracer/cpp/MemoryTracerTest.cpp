178b9eb24e327d5efbe85e68b1235bdcf6819756
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-04 15:42:06 -0300

    Test if MemoryTracer::stop() method calls muntrace

--- src/testMemoryTracer/cpp/MemoryTracerTest.cpp
+++ src/testMemoryTracer/cpp/MemoryTracerTest.cpp
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <fstream>
 #include <mcheck.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -24,6 +25,37 @@ TEST(MemoryTracerTest, startTrace) {
 
     EXPECT_EQ(0, stat(mtraceFile.c_str(), &info));
     EXPECT_TRUE(S_ISREG(info.st_mode));
+
+    unlink(mtraceFile.c_str());
+    unsetenv(envVarName.c_str());
+    free(environmentString);
+}
+
+TEST(MemoryTracerTest, stopTrace) {
+    const std::string envVarName = "MALLOC_TRACE";
+    const std::string mtraceFile = "/tmp/MemoryTracerTest_stopTrace.mtrace";
+    const std::string constEnvironmentString = envVarName + "=" + mtraceFile;
+    char* environmentString = strdup(constEnvironmentString.c_str());
+    struct stat info;
+
+    MemoryTracer tracer;
+
+    putenv(environmentString);
+
+    mtrace();
+    tracer.stop();
+
+    EXPECT_EQ(0, stat(mtraceFile.c_str(), &info));
+    EXPECT_TRUE(S_ISREG(info.st_mode));
+
+    std::ifstream traceFile(mtraceFile);
+    std::string line;
+    std::string lastLine;
+
+    while (std::getline(traceFile, line))
+        lastLine = line;
+
+    EXPECT_STREQ("= End", lastLine.c_str());
 
     unlink(mtraceFile.c_str());
     unsetenv(envVarName.c_str());
