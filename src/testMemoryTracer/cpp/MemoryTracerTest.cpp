6f613422b5af4efde3fec870b67b2f7e718e852a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-04 15:04:03 -0300

    Test that start() method calls mtrace
    - Test that after calling muntrace the expected output file is created

--- src/testMemoryTracer/cpp/MemoryTracerTest.cpp
+++ src/testMemoryTracer/cpp/MemoryTracerTest.cpp
@@ -1,3 +1,9 @@
+#include <errno.h>
+#include <mcheck.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include <gtest/gtest.h>
 
 #include "MemoryTracer.hpp"
@@ -8,4 +14,26 @@ TEST(MemoryTracerTest, classExists) {
     EXPECT_TRUE(tracer != NULL);
 
     delete tracer;
+}
+
+TEST(MemoryTracerTest, startTrace) {
+    const std::string envVarName = "MALLOC_TRACE";
+    const std::string mtraceFile = "/tmp/MemoryTracerTest_startTrace.mtrace";
+    const std::string constEnvironmentString = envVarName + "=" + mtraceFile;
+    char* environmentString = strdup(constEnvironmentString.c_str());
+    struct stat info;
+
+    MemoryTracer tracer;
+
+    putenv(environmentString);
+
+    tracer.start();
+    muntrace();
+
+    EXPECT_EQ(0, stat(mtraceFile.c_str(), &info));
+    EXPECT_TRUE(S_ISREG(info.st_mode));
+
+    unlink(mtraceFile.c_str());
+    unsetenv(envVarName.c_str());
+    free(environmentString);
 }
