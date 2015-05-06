d2c6439be067f464a0914874f5a03b14d46f5563
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-06 15:55:48 -0300

    Test setting temporary directory name prefix

--- src/test/cpp/TempDirTest.cpp
+++ src/test/cpp/TempDirTest.cpp
@@ -9,3 +9,13 @@ TEST(TempDirTest, getName) {
 
     EXPECT_TRUE(strncmp(tempDir, dir.getPath().c_str(), length) == 0);
 }
+
+TEST(TempDirTest, customDirPrefix) {
+    const char* dirPrefix = "mtrace_gtest.";
+    const int length = strlen(dirPrefix);
+    TempDir dir{dirPrefix};
+    const std::string path = dir.getPath();
+    const std::string dirName = path.substr(path.find_last_of('/') + 1);
+
+    EXPECT_TRUE(strncmp(dirPrefix, dirName.c_str(), length) == 0);
+}
